3feac55beb715a5c6284c8bcea37708dded81d86
Aadarshpandit21 <anshulkanhaiyyapandit@gmail.com>
2023-02-14 15:48:55 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let express = require("express");
+let app= express();
+
+let route = require("./Schedular.js");
+app.use("/",route);
+const router = express. Router();+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("running at "+(process.env.PORT||3000))
+ })
